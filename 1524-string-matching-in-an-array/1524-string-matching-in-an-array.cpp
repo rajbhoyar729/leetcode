21395ce8bd9f6ce5510c32c764b6db71d692843f
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-01-07 08:21:58 +0530

    Time: 0 ms (100%), Space: 11.4 MB (43.54%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,44 +1,19 @@
 class Solution {
     public:
         vector<string> stringMatching(vector<string>& words) {
-                set<string> resultSet;
-                        bool hasEmpty = false;
-                                
-                                        // Check for empty strings
-                                                for (const string& s : words) {
-                                                            if (s.empty()) {
-                                                                            hasEmpty = true;
-                                                                                            break;
-                                                                                                        }
-                                                                                                                }
-                                                                                                                        
- // Include empty string if there is at least one non-empty string
-                                                                                                    if (hasEmpty) {
-                                                                                                     for (const string& s : words) {
-                                                                                                   if (!s.empty()) {
-                                                                                       resultSet.insert("");
-  break;
-           }
-      }
-      }
-     // Sort words by length ascendingly
-                                                                                                                                                      vector<string> sortedWords = words;
-                                                                                                                                                                                                                                                                                sort(sortedWords.begin(), sortedWords.end(), [](const string& a, const string& b) {
-                                                                                                                                                                                                                                                                                            return a.length() < b.length();
-                                                                                                  });
-                                                                                               // Check for substring relationships
-                                                                                            for (size_t i = 0; i < sortedWords.size(); ++i) {
-                                                                                                  const string& s = sortedWords[i];
-   for (size_t j = i + 1; j < sortedWords.size(); ++j) {
-                                                                                                   const string& t = sortedWords[j];
-                                                                                  if (t.find(s) != string::npos) {
- resultSet.insert(s);
-         break;
-                                                                                                                    }
-                                                                                                                      }
-                                                       }
-                                                                                                                                                                           
-  // Convert set to vector
-                                                                                       vector<string> result(resultSet.begin(), resultSet.end());
-  return result;
- }                                                                                             };
+                vector<string> str;
+
+                        for (int i = 0; i < words.size(); i++) {
+                                    for (int j = 0; j < words.size(); j++) {
+                                                   if (i != j) {
+                                                                      auto found = words[j].find(words[i]);
+                                                                                         if (found != string::npos) {
+                                                                                                                str.push_back(words[i]);
+                                                                                                                                       break;
+                                                                                                                                                          }
+                                                                                                                                                                         }
+                                                                                                                                                                                     }
+                                                                                                                                                                                             }
+                                                                                                                                                                                                     return str;
+                                                                                                                                                                                                         }
+                                                                                                                                                                                                         };
