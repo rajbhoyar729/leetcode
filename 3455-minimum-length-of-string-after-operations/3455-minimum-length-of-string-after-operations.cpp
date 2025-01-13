46eddf1093288ff2b3dd22b01a714ae4d7a753f8
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-01-13 10:35:52 +0530

    Time: 7 ms (93.55%), Space: 32.2 MB (52.42%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        vector<int> cnt(26, 0);
-        int len = 0;
-        
-        for (char ch : s) {
-            cnt[ch - 'a']++;
+        vector<int> charFrequency(26, 0);
+        int totalLength = 0;
+        for (char currentChar : s) {
+            charFrequency[currentChar - 'a']++;
         }
-        
-        for (int num : cnt) {
-            if (num == 0) continue;
-            len += (num % 2 == 0) ? 2 : 1;
-        }        
-        return len;
+        for (int frequency : charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) {
+                totalLength += 2;
+            } else {
+                totalLength += 1;
+            }
+        }
+        return totalLength;
     }
 };
