2bb3f61d7c79cddea7b544c41b1d7dd4a5451eb8
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-01-13 10:33:50 +0530

    Time: 4 ms (98.39%), Space: 31.9 MB (86.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char, int> M;
+        vector<int> cnt(26, 0);
+        int len = 0;
         
-        // Step 1: Count frequency of each character in the string
-        for (char c : s) {
-            M[c]++;
+        for (char ch : s) {
+            cnt[ch - 'a']++;
         }
-
-        int delet = 0; // Variable to track total deletions
-
-        // Step 2: Iterate through the frequency map
-        for (auto pair : M) {
-            // If the frequency of a character is greater than 2
-            if (pair.second > 2) {
-                // If the frequency is even, we can remove excess characters
-                if (pair.second % 2 == 0) {
-                    delet += (pair.second - 2); // Keep 2 occurrences, remove the rest
-                } 
-                // If the frequency is odd, we need to keep 1 occurrence
-                else {
-                    delet += (pair.second - 1); // Keep 1 occurrence, remove the rest
-                }
-            }
-        }
-
-        // Step 3: Return the final length of the string after deletions
-        return s.length() - delet;
+        
+        for (int num : cnt) {
+            if (num == 0) continue;
+            len += (num % 2 == 0) ? 2 : 1;
+        }        
+        return len;
     }
 };
