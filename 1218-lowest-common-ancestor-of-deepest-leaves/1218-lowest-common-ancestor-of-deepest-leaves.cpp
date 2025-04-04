4d4be6ccb2649773b2a0ef905c0119fe0f8a61d2
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-04-04 09:05:47 +0530

    Time: 0 ms (100%), Space: 22.1 MB (39.6%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int getDepth(TreeNode* root) {
+                if (!root)
+                            return 0;
+                                    return max(getDepth(root->right), getDepth(root->left)) + 1;
+                                        }
+                                            
+                                                TreeNode* lcaDeepestLeaves(TreeNode* root) {
+                                                        if (!root)
+                                                                    return NULL;
+                                                                            
+                                                                                    int right_depth = getDepth(root->right);
+                                                                                            int left_depth = getDepth(root->left);
+                                                                                                    
+                                                                                                            if (right_depth == left_depth)
+                                                                                                                        return root;
+                                                                                                                                
+                                                                                                                                        if (right_depth > left_depth) 
+                                                                                                                                                    return lcaDeepestLeaves(root->right);
+                                                                                                                                                            
+                                                                                                                                                                    else 
+                                                                                                                                                                                return lcaDeepestLeaves(root->left);
+                                                                                                                                                                                    }
+                                                                                                                                                                                    };
