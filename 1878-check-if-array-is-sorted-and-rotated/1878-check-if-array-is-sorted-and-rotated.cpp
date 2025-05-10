c72e543a61428b257b8a22a0eaaf0bdba76c57ac
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-05-10 08:34:31 +0530

    Time: 3 ms (2.6%), Space: 11.9 MB (5.59%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,12 +1,37 @@
 class Solution {
- public:
-  bool check(vector<int>& nums) {
-    const int n = nums.size();
-    int rotates = 0;
-    for (int i = 0; i < n; ++i){
-      if (nums[i] > nums[(i + 1) % n] && ++rotates > 1)
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+
+        // Construct the rotated array
+        vector<int> checkSorted(n);
+
+        // Iterate through all possible rotation offsets
+        for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
+            int currIndex = 0;
+            for (int index = rotationOffset; index < n; ++index) {
+                checkSorted[currIndex++] = (nums[index]);
+            }
+            for (int index = 0; index < rotationOffset; ++index) {
+                checkSorted[currIndex++] = (nums[index]);
+            }
+
+            // Check if the constructed array is sorted
+            bool isSorted = true;
+            for (int index = 0; index < n - 1; ++index) {
+                if (checkSorted[index] > checkSorted[index + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            // If sorted, return true
+            if (isSorted) {
+                return true;
+            }
+        }
+
+        // If no rotation makes the array sorted, return false
         return false;
     }
-    return true;
-  }
 };
