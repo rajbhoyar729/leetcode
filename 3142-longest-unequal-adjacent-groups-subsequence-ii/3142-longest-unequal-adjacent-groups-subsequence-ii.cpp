3d589221e3613217168a5153c8d1d2c296203d93
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-05-16 13:46:47 +0530

    Time: 37 ms (87.5%), Space: 34.3 MB (93.75%) - LeetHub

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.cpp
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -1,38 +1,35 @@
 class Solution {
-    public:
-        static bool hamming1(string& s, string& t){
-                const int sz=s.size();
-                        if (sz!=t.size()) return 0;
-                                int diff=0;
-                                        for (int i=0; i<sz && diff<2; i++)
-                                                    diff+=s[i]!= t[i];
-                                                            return diff== 1;
-                                                                }
-
-                                                                    static vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
-                                                                            const int n=words.size();
-                                                                                    int maxLen=0, pos=-1;
-                                                                                            int* dp=(int*)alloca(n*4);
-                                                                                                    fill(dp, dp+n, 1);
-                                                                                                            int* prev=(int*)alloca(n*4);
-                                                                                                                    fill(prev, prev+n, -1);
-                                                                                                                            for(int i=0; i<n; i++){
-                                                                                                                                        for(int j=0; j<i; j++){
-                                                                                                                                                        if(groups[i]!=groups[j] && hamming1(words[i], words[j]) 
-                                                                                                                                                                        && dp[j]+1>dp[i]){
-                                                                                                                                                                                            dp[i]=dp[j]+1;
-                                                                                                                                                                                                                prev[i]=j;
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                        if(dp[i]>maxLen){
-                                                                                                                                                                                                                                                                        maxLen=dp[i];
-                                                                                                                                                                                                                                                                                        pos=i;
-                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                    vector<string> ans(maxLen);
-                                                                                                                                                                                                                                                                                                                            for(; pos!=-1; pos=prev[pos]){
-                                                                                                                                                                                                                                                                                                                                        ans[--maxLen]=words[pos];
-                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                        return ans;
-                                                                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                                                            };
+    bool check(string &x1 , string &x2){
+        if(x1.length() != x2.length()) return false ;
+        int cnt = 0 ;
+        for(int i = 0 ; i < x1.length() ; i++){
+            cnt += x1[i] != x2[i] ;
+            if(cnt > 1) return false ;
+        }
+        return cnt == 1 ;
+    }
+public:
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        const int n = groups.size() ;
+        vector<int> prev(n,-1); 
+        vector<int> dp(n,1) ;
+        int max_len = 1 , max_idx = 0 ;
+        for(int i = 1 ; i < n ; i++){
+            int idx = -1 ;
+            for(int j = i - 1 ; j >= 0 ; j--){
+                if(groups[i] != groups[j] && (idx == -1 || dp[j] > dp[idx]) && check(words[i],words[j])){
+                    idx = j ;
+                }
+            }
+            if(idx != -1) dp[i] += dp[idx] , prev[i] = idx ;
+            if(dp[i] > max_len) max_len = dp[i] , max_idx = i ;
+        }
+        int curr = max_idx , res_idx = max_len - 1 ;
+        vector<string> res(max_len) ;
+        while(curr != -1){
+            res[res_idx--] = words[curr] ;
+            curr = prev[curr] ;
+        }
+        return res ;
+    }
+};
