d1cedd9caef31100d1e1e010241ec3bd7be62083
Raj bhoyar <85496077+rajbhoyar729@users.noreply.github.com>
2025-10-08 08:14:41 +0530

    Time: 35 ms (95.05%), Space: 125 MB (94.62%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    static vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int freq[100001]={0}, pMax=0;
-        for(int p: potions){
-            freq[p]++;
-            pMax=max(pMax, p);
+    static inline int binarySearch(int k, vector<int>& potions, int m){
+        int l=0, r=m-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(potions[mid]>= k)
+                r=mid;
+            else
+                l=mid+1;
         }
-        // freq = prefix sum for freq 
-        partial_sum( freq, freq+pMax+1, freq);
-
-        const int n=spells.size(), m=potions.size();
+        return l;
+    }
+    static vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        const int n=spells.size();
         vector<int> result(n, 0);
         
+        const int m=potions.size();
+        const int maxPotion=potions[m - 1];
+
+        
         for (int i=0; i<n; i++) {
-            const int spell=spells[i];
-            const long long k = (success+spell-1)/spell;
-            if (k<=pMax) {
-                result[i]=m-(k>=1?freq[k-1]:0);
+            int spell=spells[i];
+            long long k=(success+spell-1)/spell;
+            if (k <=maxPotion) {
+                result[i]=m-binarySearch(k, potions, m);
             }
         }       
         return result;
